189fb2605de7a7a762318d7978bf39cf340d9858
Rannojit <cserannojitsarkar22@gmail.com>
2021-09-15 12:43:20 +0600

    .

--- themes/jm/header.php
+++ themes/jm/header.php
@@ -31,79 +31,85 @@ $email = get_field('emailaddres', 'options');
 <!-- Main Header-->
 <header class="main-header">
     <div class="hdr-bdr"></div>
-  <!-- Header Upper -->
-    <div class="header-upper">        
-        <div class="auto-container">
-            <!-- Main Box -->
-            <div class="main-box clearfix">
-                <!--Logo-->
-                <div class="logo-box">
-                  <?php if( !empty($logo_tag) ): ?>
-                  <div class="logo">
-                    <a href="<?php echo esc_url(home_url('/')); ?>">
-                      <?php echo $logo_tag; ?>
-                    </a>
-                  </div>
-                  <?php endif; ?>
-                </div>
-                <div class="upper-right">
-                    <div class="hdr-bdr-content"></div>
-                    <div class="top-info clearfix">
-                        <ul class="clearfix">
-                            <?php if( !empty($whatsapp) ): ?>
-                            <li class="phone"><span class="icon"><img src="<?php echo THEME_URI; ?>/assets/images/whats-app-icon.png" alt=""></span><span class="subtitle">Whatsapp now</span><a href="https://wa.me/<?php echo phone_preg($whatsapp); ?>"><?php echo $whatsapp; ?></a></li>
-                            <?php endif; ?>
-                            <?php if( !empty($telephone) ): ?>
-                            <li class="phone"><span class="icon"><img src="<?php echo THEME_URI; ?>/assets/images/tell-icon.png" alt=""></span><span class="subtitle">CALL NOW</span><a href="tel:<?php echo phone_preg($telephone); ?>"><?php echo $telephone; ?></a></li>
-                            <?php endif; ?>
-                            <?php if( !empty($email) ): ?>
-                            <li class="email"><span class="icon"><img src="<?php echo THEME_URI; ?>/assets/images/mail-icon.png" alt=""></span><span class="subtitle">EMAIL</span><a href="mailto:<?php echo $email; ?>"><?php echo $email; ?></a></li>
-                            <?php endif; ?>
-                        </ul>
-                        <div class="serach-box">
-                          <form method="get" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-                            <input type="text" placeholder="Search" name="s" value="<?php echo get_search_query(); ?>">
-                            <input type="hidden" name="post_type" value="product" />
-                          </form>
+    <div class="container-lg">
+      <div class="row">
+        <div class="col-md-12">
+              <!-- Header Upper -->
+                <div class="header-upper">        
+                    <div class="auto-container">
+                        <!-- Main Box -->
+                        <div class="main-box clearfix">
+                            <!--Logo-->
+                            <div class="logo-box">
+                              <?php if( !empty($logo_tag) ): ?>
+                              <div class="logo">
+                                <a href="<?php echo esc_url(home_url('/')); ?>">
+                                  <?php echo $logo_tag; ?>
+                                </a>
+                              </div>
+                              <?php endif; ?>
+                            </div>
+                            <div class="upper-right">
+                                <div class="hdr-bdr-content"></div>
+                                <div class="top-info clearfix">
+                                    <ul class="clearfix">
+                                        <?php if( !empty($whatsapp) ): ?>
+                                        <li class="phone"><span class="icon"><img src="<?php echo THEME_URI; ?>/assets/images/whats-app-icon.png" alt=""></span><span class="subtitle">Whatsapp now</span><a href="https://wa.me/<?php echo phone_preg($whatsapp); ?>"><?php echo $whatsapp; ?></a></li>
+                                        <?php endif; ?>
+                                        <?php if( !empty($telephone) ): ?>
+                                        <li class="phone"><span class="icon"><img src="<?php echo THEME_URI; ?>/assets/images/tell-icon.png" alt=""></span><span class="subtitle">CALL NOW</span><a href="tel:<?php echo phone_preg($telephone); ?>"><?php echo $telephone; ?></a></li>
+                                        <?php endif; ?>
+                                        <?php if( !empty($email) ): ?>
+                                        <li class="email"><span class="icon"><img src="<?php echo THEME_URI; ?>/assets/images/mail-icon.png" alt=""></span><span class="subtitle">EMAIL</span><a href="mailto:<?php echo $email; ?>"><?php echo $email; ?></a></li>
+                                        <?php endif; ?>
+                                    </ul>
+                                    <div class="serach-box">
+                                      <form method="get" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+                                        <input type="text" placeholder="Search" name="s" value="<?php echo get_search_query(); ?>">
+                                        <input type="hidden" name="post_type" value="product" />
+                                      </form>
+                                    </div>
+                                </div>
+                                <!--Nav Outer-->
+                                <div class="nav-outer clearfix">                
+                                    <nav class="main-menu navbar-expand-md navbar-light">
+                                        <div class="navbar-header">
+                                            <!-- Toggle Button -->
+                                            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                                                <span class="icon"><img src="<?php echo THEME_URI; ?>/assets/images/icons/menu-icon.png" alt=""></span>
+                                            </button>
+                                        </div>
+                                        
+                                        <div class="collapse navbar-collapse clearfix" id="navbarSupportedContent">
+                                          <?php 
+                                            $menuOptions = array( 
+                                                'theme_location' => 'cbv_main_menu', 
+                                                'menu_class' => 'navigation clearfix',
+                                                'container' => '',
+                                                'container_class' => ''
+                                              );
+                                            wp_nav_menu( $menuOptions ); 
+                                          ?>
+                                        </div>
+                                    </nav>
+                                    <!-- Main Menu End-->
+
+                                </div>
+                                <!--Nav Outer End-->
+                                <!-- Hidden Nav Toggler -->
+                                <div class="nav-toggler">
+                                    <button class="hidden-bar-opener"><span class="icon"><img src="<?php echo THEME_URI; ?>/assets/images/icons/menu-icon.png" alt=""></span></button>
+                                </div><!-- / Hidden Nav Toggler -->
+
+                            </div>
                         </div>
                     </div>
-                    <!--Nav Outer-->
-                    <div class="nav-outer clearfix">                
-                        <nav class="main-menu navbar-expand-md navbar-light">
-                            <div class="navbar-header">
-                                <!-- Toggle Button -->
-                                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                                    <span class="icon"><img src="<?php echo THEME_URI; ?>/assets/images/icons/menu-icon.png" alt=""></span>
-                                </button>
-                            </div>
-                            
-                            <div class="collapse navbar-collapse clearfix" id="navbarSupportedContent">
-                              <?php 
-                                $menuOptions = array( 
-                                    'theme_location' => 'cbv_main_menu', 
-                                    'menu_class' => 'navigation clearfix',
-                                    'container' => '',
-                                    'container_class' => ''
-                                  );
-                                wp_nav_menu( $menuOptions ); 
-                              ?>
-                            </div>
-                        </nav>
-                        <!-- Main Menu End-->
-
-                    </div>
-                    <!--Nav Outer End-->
-                    <!-- Hidden Nav Toggler -->
-                    <div class="nav-toggler">
-                        <button class="hidden-bar-opener"><span class="icon"><img src="<?php echo THEME_URI; ?>/assets/images/icons/menu-icon.png" alt=""></span></button>
-                    </div><!-- / Hidden Nav Toggler -->
-
-                </div>
+                    <!-- End Header Upper -->
+                    
+                </div>    
             </div>
-        </div>
-        <!-- End Header Upper -->
-        
-    </div>    
+      </div>
+  </div>
 </header>
 <!--End Main Header -->
 
